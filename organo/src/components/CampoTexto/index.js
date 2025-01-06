38df647c95c962327a21d731747c44df0c84d788
jpars26 <jpars131@gmail.com>
2025-01-06 14:35:04 -0300

    add CampoTexto, ListaSuspensa, and Botao components with styling; implement form submission handling

--- organo/src/components/CampoTexto/index.js
+++ organo/src/components/CampoTexto/index.js
@@ -1,11 +1,24 @@
 import './CampoTexto.css';
 
 const CampoTexto = (props) => {
+    const placeholderModificado = `${props.placeholder}...`;
+
+    let valor = 'Joao Paulo';
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(valor);
+    }
     
     return (
         <div className="campo-texto">
-            <label>{props.label}</label>
-            <input type="text" placeholder={props.placeholder} />
+            <label>
+
+                {props.label}
+            
+            </label>
+            <input required={props.obrigatorio} onChange={aoDigitado} placeholder={props.placeholder} />
+            
         </div>
     )
 }
--- organo/src/components/Formulario/index.js
+++ organo/src/components/Formulario/index.js
@@ -1,15 +1,29 @@
 import './Formulario.css';
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botoes';
 
 const Formulario = () => {
+
+    const times = ['Time 1', 'Time 2', 'Time 3', 'Time 4', 'Time 5'];
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault();
+        console.log('Salvando dados');
+    }
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit= {aoSalvar}>
                 <h2>Preencha os dados do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome"/>
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo"/>
                 <CampoTexto label="Imagem" placeholder="Digite seu endereço de imagem"/>
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}></ListaSuspensa>
+                <Botao type="submit">
+                    Criar Card
+                </Botao>
             </form>
+            
         </section>
     )
 }
